97983aa7584cc096cf82f4204b76b87387b3e7b7
인다운 <incrying@duksung.ac.kr>
2023-11-25 15:29:56 +0900

    [1125] dw more_playlist fix

--- script/more_playlist.js
+++ script/more_playlist.js
@@ -46,3 +46,92 @@ function pageLink() {
   // Optionally, provide some feedback to the user
   alert("현재 페이지 주소가 클립보드에 복사되었습니다.");
 }
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var btn = document.querySelector(".create-playlist button");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks the button, open the modal
+  btn.onclick = function () {
+    modal.style.display = "block";
+  };
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+
+  // Optional: You can add more functionality to handle form submission and playlist creation
+  document
+    .getElementById("playlistForm")
+    .addEventListener("submit", function (e) {
+      e.preventDefault();
+      // Add logic to create a new playlist here
+      // You can use JavaScript to send a request to the server or update the UI as needed
+      // After creating the playlist, you may want to close the modal
+      modal.style.display = "none";
+    });
+});
+
+function cancelAttachment() {
+  // Reset the file input value to clear the selected file
+  var thumbnailInput = document.getElementById("thumbnailInput");
+  if (thumbnailInput) {
+    thumbnailInput.value = "";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var fileInput = document.getElementById("file");
+  var uploadNameInput = document.querySelector(".upload-name");
+  var cancelFileButton = document.getElementById("cancelFile");
+
+  // Add a change event listener to the file input
+  if (fileInput) {
+    fileInput.addEventListener("change", function () {
+      var selectedFile = fileInput.files[0];
+      if (selectedFile) {
+        uploadNameInput.value = selectedFile.name;
+      } else {
+        uploadNameInput.value = "";
+      }
+    });
+  }
+
+  // Add a click event listener to the cancelFile button
+  if (cancelFileButton) {
+    cancelFileButton.addEventListener("click", function () {
+      // Reset the file input by cloning and replacing it
+      if (fileInput) {
+        var newFileInput = fileInput.cloneNode(true);
+        fileInput.parentNode.replaceChild(newFileInput, fileInput);
+
+        // Clear the upload-name input
+        uploadNameInput.value = "";
+      }
+    });
+  }
+});
+
+function toggleButtonText() {
+  var button = document.getElementById('addButton');
+  if (button.textContent === '내 Playlist에 추가하기') {
+    button.textContent = '추가됨! (재클릭 시 취소)';
+  } else {
+    button.textContent = '내 Playlist에 추가하기';
+  }
+}
